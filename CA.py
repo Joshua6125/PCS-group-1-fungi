dd14b1d170f3a067382c90cba1f3daba4498a497
DCodePro <duco@vanwallenberg.com>
2026-01-05 16:47:04 +0100

    Different architecture of the CA - Still deterministic no neighbours

--- CA.py
+++ CA.py
@@ -1,4 +1,5 @@
 from itertools import product
+import numpy as np
 
 # State Names
 EMPTY = 0
@@ -11,54 +12,61 @@ DECAYING = 6
 DEAD1 = 7
 DEAD2 = 8
 INERT = 9
-
+        
 class CA():
-    def __init__(self, x: int, y: int, state: int):
-        self.x: int = x
-        self.y: int = y
-        self.state: int = state
-        
-    def transition(self):
-        if self.state == SPORE:
-            self.state = YOUNG
-        elif self.state ==  YOUNG:
-            self.state = MATURING
-        elif self.state == MATURING:
-            self.state = MUSHROOMS
-        elif self.state == OLDER:
-            self.state = DECAYING
-        elif self.state == MUSHROOMS:
-            self.state = DECAYING
-        elif self.state == DECAYING:
-            self.state = DEAD1
-        elif self.state == DEAD1:
-            self.state = DEAD2
-        elif self.state == DEAD2:
-            self.state = EMPTY
-        elif self.state == EMPTY:
-            self.state = YOUNG
-        elif self.state == INERT:
-            self.state = INERT
-        else:
-            raise ValueError
-    
-    def __repr__(self):
-        return str(self.state)
-        
-
-class CA_Grid():
     def __init__(self, n: int):
         self.n: int = n
-        self.grid: list[list[CA]] = [[CA(x, y, 0) for x in range(n)] for y in range(self.n)]
+        self.grids: list[np.array] = [np.zeros((n, n), dtype=np.uint32)]
+        self.time = 0
         
     def __repr__(self):
         message = ""
         for y in range(self.n):
             for x in range(self.n):
-                message += str(self.grid[y][x]) + " "
+                message += str(self.grids[-1][y][x]) + " "
             message += "\n"
         return message
         
     def step(self):
+        grid = np.zeros((self.n, self.n), dtype=np.uint32)
         for (x, y) in product(range(self.n), repeat=2):
-            self.grid[y][x].transition()
+            grid[y][x] = self.transition(self.grids[-1][y][x])
+        self.grids.append(grid)
+            
+    def set_state(self, x: int, y: int, state: int, time: int=0):
+        """
+        Set the state of a single cell value
+        
+        :param self: Description
+        :param x: 0 indexed coordinate
+        :type x: int
+        :param y: 0 indexed coordinate
+        :type y: int
+        :param state: Description
+        :type state: int
+        """
+        self.grids[time][y][x] = state
+        
+    def transition(self, state):
+        if state ==  SPORE:
+            return YOUNG
+        elif state ==   YOUNG:
+            return MATURING
+        elif state ==  MATURING:
+            return MUSHROOMS
+        elif state ==  OLDER:
+            return DECAYING
+        elif state ==  MUSHROOMS:
+            return DECAYING
+        elif state ==  DECAYING:
+            return DEAD1
+        elif state ==  DEAD1:
+            return DEAD2
+        elif state ==  DEAD2:
+            return EMPTY
+        elif state ==  EMPTY:
+            return YOUNG
+        elif state ==  INERT:
+            return INERT
+        else:
+            raise ValueError
