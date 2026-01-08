269a411cfeed34a4094cf5a2362ae814ab4000d8
taeke.roukema <taeke.roukema@student.uva.nl>
2026-01-08 11:07:56 +0100

    Print with emojis

--- main.py
+++ main.py
@@ -13,16 +13,31 @@ DEAD1 = 7
 DEAD2 = 8
 INERT = 9
 
+state_emojis = {
+    EMPTY: "⬛",
+    SPORE: "✨",
+    YOUNG: "👶",
+    MATURING: "👦",
+    MUSHROOMS: "🍄",
+    OLDER: "🧑",
+    DECAYING: "👴",
+    DEAD1: "💀",
+    DEAD2: "💀️",
+    INERT: "🟥"
+}
+
 # Parameters
-n = 5
+n = 20
 prob_spore_to_hyphae = 0.2
 prob_mushroom = 0.5
 prob_spread = 0.5
 
 # Main code
 simulation = CA(n, prob_spore_to_hyphae, prob_mushroom, prob_spread)
-simulation.set_state(0, 1, INERT)
 print(simulation)
 for _ in range(10):
     simulation.step()
-    print(simulation)
+    sim_string = str(simulation)
+    for key in state_emojis.keys():
+        sim_string = sim_string.replace(str(key), state_emojis[key])
+    print(sim_string)
