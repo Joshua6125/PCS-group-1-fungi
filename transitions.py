6764c1e4d289bd2f26c6133a764c1fc2937b1640
joshua <joshuavanrooij@gmail.com>
2026-01-08 13:08:04 +0100

    Made code more readable.

--- transitions.py
+++ transitions.py
@@ -2,15 +2,12 @@ from CA import CA
 from constants import *
 import numpy as np
 
-VON_NEUMANN_NBD = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
 class BasicSim(CA):
     def __init__(self, n: int, prob_spore_to_hyphae: float, prob_mushroom: float, prob_spread: float):
         super().__init__(n)
         self.prob_spore_to_hyphae = prob_spore_to_hyphae
         self.prob_mushroom = prob_mushroom
         self.prob_spread = prob_spread
-
 
     def transition(self, grid: list[np.ndarray], x: int, y: int) -> int:
         state = grid[y][x]
@@ -44,8 +41,12 @@ class BasicSim(CA):
             return EMPTY
 
         if state == EMPTY:
-            if any(grid[y + dy][x + dx] == YOUNG if 0 <= y + dy < self.n and 0 <= x + dx < self.n else False for (dx, dy) in VON_NEUMANN_NBD):
-                if np.random.random() < self.prob_spread:
+            for (dx, dy) in MOORE_NBD:
+                if not (0 <= y + dy < self.n and 0 <= x + dx < self.n):
+                    continue
+                if not grid[y + dy][x + dx] == YOUNG:
+                    continue
+                if np.random.random() < self.prob_spread/np.linalg.norm((dx, dy)):
                     return YOUNG
             return EMPTY
 
