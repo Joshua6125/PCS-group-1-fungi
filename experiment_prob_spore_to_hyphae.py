b77c9174ca2d8ec3c5283d38b645c074a083f74e
joshua <joshuavanrooij@gmail.com>
2026-01-09 14:48:17 +0100

    Fix for experiment to adjust for new refactoring

--- experiment_prob_spore_to_hyphae.py
+++ experiment_prob_spore_to_hyphae.py
@@ -1,6 +1,6 @@
 import numpy as np
 from transitions import BasicSim
-import time
+from utils import gkern
 from matplotlib import pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
@@ -17,24 +17,32 @@ DEAD2 = 8
 INERT = 9
 
 # Parameters
-n = 75
-num_iterations = 50
-prob_spore_to_hyphae = 1
-prob_mushroom = 0.9
+sim_parameters = {
+        "n": 75,
+        "prob_spore_to_hyphae": 1.0,
+        "prob_mushroom": 0.7,
+        "prob_spread": 0.5,
+        "toxin_threshold": 0.3,
+        "toxin_decay": 0.05,
+        "toxin_convolution": gkern(5, 1, 1)
+}
+
+num_iterations = 25
 
 colors = [(0, 1, 0), (0, 0.5, 0.5), (0, 0, 0.5), (0, 0, 1), (1, 0, 0), (0.5, 0.5, 0), (0, 0, 0), (0, 0, 0), (1, 1, 1)]
 
 # Main code
 probabilities = np.linspace(0.1, 1, 10)
 for probability in probabilities:
-    simulation = BasicSim(n, prob_spore_to_hyphae, prob_mushroom, probability)
-    simulation.set_state(n//2, n//2, SPORE)
+    sim_parameters["prob_spread"] = probability
+    simulation = BasicSim(sim_parameters)
+    simulation.set_state(sim_parameters["n"]//2, sim_parameters["n"]//2, SPORE)
     for _ in range(num_iterations):
         simulation.step()
     cmap = LinearSegmentedColormap.from_list("cmap_name", colors, N=10)
-    
+
     fig, ax = plt.subplots()
-    ax.imshow(simulation.grids[-1], origin='lower', cmap=cmap)
-    
-    plt.title(f"Fairy rings ater {num_iterations} steps with spread probability {probability}")
-    plt.show()
+    ax.imshow(simulation.state_grids[-1], origin='lower', cmap=cmap)
+
+    plt.title(f"Fairy rings ater {num_iterations} steps with spread probability {round(probability, 1)}")
+    plt.savefig(f"./plots/experiment_prob_spread/{num_iterations}_{round(probability, 1)}.png")
