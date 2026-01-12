066d805d7497a4eb1d92a9ec59a6c90e719f305b
DCodePro <duco@vanwallenberg.com>
2026-01-12 17:02:46 +0100

    Experiment with more test data

--- experiment_validty_hull.py
+++ experiment_validty_hull.py
@@ -2,6 +2,7 @@ from constants import SPORE
 from transitions import BasicSim, BasicToxinSim
 from matplotlib import pyplot as plt
 from utils import gkern
+import numpy as np
 
 
 def main():
@@ -17,19 +18,37 @@ def main():
         "iterations": 50
     }
 
-    num_simulations = 100
-    vals = []
-    for i in range(num_simulations):
+    decay_rates = np.linspace(0, 0.1, 40)
+    num_simulations = 40
+    val_per_rate = []
+    val_per_rate_lower = []
+    val_per_rate_upper = []
+
+    for i, rate in enumerate(decay_rates):
         print(i)
-        simulation = BasicToxinSim(sim_parameters)
-        simulation.set_state(sim_parameters["n"]//2, sim_parameters["n"]//2, SPORE)
+        sim_parameters["toxin_decay"] = rate
+        vals = []
+        for _ in range(num_simulations):
+            simulation = BasicToxinSim(sim_parameters)
+            simulation.set_state(sim_parameters["n"]//2, sim_parameters["n"]//2, SPORE)
 
-        for _ in range(sim_parameters["iterations"]):
-            simulation.step()
-        vals.append(simulation.inner_ring_detector())
+            for _ in range(sim_parameters["iterations"]):
+                simulation.step()
+            vals.append(simulation.inner_ring_detector())
 
-    plt.hist(vals)
-    plt.savefig("result_medium_toxin_decay.png")
+        mean_val = np.mean(vals)
+        std_val = np.std(vals)
+        val_per_rate.append(mean_val)
+        val_per_rate_lower.append(mean_val - std_val)
+        val_per_rate_upper.append(mean_val + std_val)
+    plt.plot(decay_rates, val_per_rate, )
+    plt.plot(decay_rates, val_per_rate_lower)
+    plt.plot(decay_rates, val_per_rate_upper)
+    plt.title("relative size inner rings per decay rate")
+    plt.xlabel("decay rate")
+    plt.ylabel("size convex hull divided by total")
+    plt.legend(["mean", "lower", "upper"])
+    plt.savefig("results_10_simulations.png")
 
 
 if __name__ == "__main__":
