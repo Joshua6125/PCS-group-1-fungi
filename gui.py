feef5fd51347271a9a477643d2483612a4fef675
joshua <joshuavanrooij@gmail.com>
2026-01-14 12:32:47 +0100

    Added probabilistic toxins

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ from config import SPORE, sim_parameters
 import threading
 import queue
 
-from transitions import BasicToxinSim
+from transitions import ProbToxinSim
 
 # Implement the default Matplotlib key bindings.
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -17,7 +17,7 @@ from matplotlib.colors import ListedColormap
 colors = [(0, 0.4, 0), (1, 1, 1), (1, .8, .8), (1, .4, .4),
           (.8, 0, 0), (0.4, 0.2, 0), (.4, .4, 0), (.1, .2, 0), (.1, .2, 0)]
 
-sim = BasicToxinSim(sim_parameters)
+sim = ProbToxinSim(sim_parameters)
 sim.set_state(sim_parameters["n"]//2, sim_parameters["n"]//2, SPORE)
 
 
