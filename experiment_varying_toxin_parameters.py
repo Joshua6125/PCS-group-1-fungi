1022e9b1bf201e43ca765b5ef323711f1028fa01
taeke.roukema <taeke.roukema@student.uva.nl>
2026-01-26 17:03:13 +0100

    Add percentages to ticks

--- experiment_varying_toxin_parameters.py
+++ experiment_varying_toxin_parameters.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.ticker as ticker
 from concurrent.futures import ProcessPoolExecutor
 from config import SPORE, sim_parameters
 from transitions import ProbToxinSim
@@ -69,7 +70,8 @@ def main():
     heatmap_data = [[100 * j for j in i] for i in heatmap_data]
     im = plt.imshow(heatmap_data, cmap="vanimo")
     plt.colorbar(im, orientation="vertical",
-                 label="% of simulations forming FFR without inner ring")
+                 label="% of simulations forming FFR without inner ring").ax.yaxis.set_major_formatter(ticker.PercentFormatter())
+    
     plt.title("FFR prevalence for varying kernel variance and decay values.")
     plt.ylabel("Variance")
     plt.yticks(range(0, len(variances), 2), labels=[
