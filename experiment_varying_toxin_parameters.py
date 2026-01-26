1903395c7ac0608b0802f24f8c2fffa8e10fd149
taeke.roukema <taeke.roukema@student.uva.nl>
2026-01-26 16:08:03 +0100

    some hints, sum style

--- experiment_varying_toxin_parameters.py
+++ experiment_varying_toxin_parameters.py
@@ -6,7 +6,12 @@ from transitions import ProbToxinSim
 from utils import gkern
 import itertools
 
-def run_single_simulation(kernel_variance, decay, params, num_iterations):
+
+def run_single_simulation(
+        kernel_variance: float,
+        decay: float,
+        params,
+        num_iterations: int):
     params["toxin_convolution_variance"] = kernel_variance
     params["toxin_decay"] = decay
     simulation = ProbToxinSim(params)
@@ -19,6 +24,7 @@ def run_single_simulation(kernel_variance, decay, params, num_iterations):
     if detect == None:
         return 0
     return simulation.inner_ring_detector()[0]
+
 
 def main():
     variances = np.arange(0.01, 1.2, 0.05)
@@ -34,11 +40,17 @@ def main():
         heatmap_data = np.zeros((len(variances), len(decays)))
 
         with ProcessPoolExecutor() as executor:
-            for (n, var), (m, decay) in itertools.product(enumerate(variances), enumerate(decays)):
+            for (n, var), (m, decay) in itertools.product(
+                    enumerate(variances),
+                    enumerate(decays)):
                 print(f"calculating variance: {var}, decay: {decay}")
 
-                futures = [executor.submit(run_single_simulation, var, decay, sim_parameters, num_iterations)
-                        for _ in range(num_simulations)]
+                futures = [executor.submit(
+                    run_single_simulation,
+                    var,
+                    decay,
+                    sim_parameters,
+                    num_iterations) for _ in range(num_simulations)]
 
                 vals = [f.result() for f in futures]
 
@@ -61,14 +73,18 @@ def main():
             heatmap_data = eval(fr_p_file.read())
     heatmap_data = [[100 * j for j in i] for i in heatmap_data]
     im = plt.imshow(heatmap_data, cmap="vanimo")
-    plt.colorbar(im, orientation="vertical", label="% of simulations forming FFR without inner ring")
+    plt.colorbar(im, orientation="vertical",
+                 label="% of simulations forming FFR without inner ring")
     plt.title("FFR prevalence for varying kernel variance and decay values.")
     plt.ylabel("Variance")
-    plt.yticks(range(0, len(variances), 2), labels=[f"{x:.2f}" for x in variances[::2]])
+    plt.yticks(range(0, len(variances), 2), labels=[
+               f"{x:.2f}" for x in variances[::2]])
     plt.xlabel("Decay")
-    plt.xticks(range(0, len(decays), 2), labels=[f"{x:.2f}" for x in decays[::2]], rotation=45)
+    plt.xticks(range(0, len(decays), 2), labels=[
+               f"{x:.2f}" for x in decays[::2]], rotation=45)
     plt.savefig("./plots/experiment_varying_toxin_parameters.png")
     plt.show()
+
 
 if __name__ == "__main__":
     main()
