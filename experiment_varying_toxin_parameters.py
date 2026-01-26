2ce9a6fb001fe0bf12ddbc39fcd88de37141d6ba
taeke.roukema <taeke.roukema@student.uva.nl>
2026-01-26 11:46:20 +0100

    Don't vary kernel size but instead decay

--- experiment_varying_toxin_parameters.py
+++ experiment_varying_toxin_parameters.py
@@ -6,9 +6,9 @@ from transitions import ProbToxinSim
 from utils import gkern
 import itertools
 
-def run_single_simulation(variance, kernel_size, params, num_iterations):
-    params["toxin_convolution_variance"] = variance
-    params["toxin_convolution_size"] = kernel_size
+def run_single_simulation(kernel_variance, decay, params, num_iterations):
+    params["toxin_convolution_variance"] = kernel_variance
+    params["toxin_decay"] = decay
     simulation = ProbToxinSim(params)
     simulation.set_state(params["n"] // 2, params["n"] // 2, SPORE)
 
@@ -21,19 +21,19 @@ def run_single_simulation(variance, kernel_size, params, num_iterations):
     return simulation.inner_ring_detector()[0]
 
 def main():
-    variances = np.arange(0.01, 1, 0.05)
-    kernel_sizes = np.arange(1, 10, 2)
+    variances = np.arange(0.01, .6, 0.1)
+    decays = np.arange(0, .1, 0.01)
     num_simulations = 20
     num_iterations = 50
 
     fairy_ring_vars = []
-    fairy_ring_kern_sizes = []
+    fairy_ring_decays = []
 
     with ProcessPoolExecutor() as executor:
-        for var, kern_size in itertools.product(variances, kernel_sizes):
-            print(f"calculating var: {var}, kern_size: {kern_size}")
+        for var, decay in itertools.product(variances, decays):
+            print(f"calculating variance: {var}, decay: {decay}")
 
-            futures = [executor.submit(run_single_simulation, var, kern_size, sim_parameters, num_iterations)
+            futures = [executor.submit(run_single_simulation, var, decay, sim_parameters, num_iterations)
                        for _ in range(num_simulations)]
 
             vals = [f.result() for f in futures]
@@ -44,19 +44,18 @@ def main():
             print(fairy_ring_ratio)
             if fairy_ring_ratio >= 0.5:
                 print(f"FAIRY RING DETECTED")
-                np.append(fairy_ring_kern_sizes, kern_size)
                 fairy_ring_vars.append(var)
-                fairy_ring_kern_sizes.append(kern_size)
+                fairy_ring_decays.append(decay)
             else:
                 print("no fairy ring detected")
 
-    print(fairy_ring_vars, fairy_ring_kern_sizes)
-    plt.scatter(fairy_ring_vars, fairy_ring_kern_sizes)
-    plt.title("Do fairy rings show for diffusion parameters")
+    print(fairy_ring_vars, fairy_ring_decays)
+    plt.scatter(fairy_ring_vars, fairy_ring_decays)
+    plt.title("Do fairy rings show for toxic parameters")
     plt.xlabel("Variance")
-    plt.ylabel("Kernel Size")
+    plt.ylabel("Decay")
     plt.legend()
-    plt.savefig("./plots/experiment_varying_kernel.png")
+    plt.savefig("./plots/experiment_varying_toxin_parameters.png")
     plt.show()
 
 if __name__ == "__main__":
